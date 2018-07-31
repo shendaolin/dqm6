66509cbf0daaf90cfad72cb386e4192daa1fd108
sdlin <shendl@winning.com.cn>
2018-07-31 08:58:54 +0800

    create

---
+++ src/main/java/cn/com/winning/dqm6/Dqm6Application.java
@@ -0,0 +1,12 @@
+package cn.com.winning.dqm6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dqm6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dqm6Application.class, args);
+	}
+}
---
+++ src/test/java/cn/com/winning/dqm6/Dqm6ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.winning.dqm6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dqm6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
